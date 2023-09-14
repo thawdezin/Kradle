405b959477fd866f3737331fc36e0415b3590950
Thaw De Zin <thawdezin@gmail.com>
2023-09-14 15:09:35 +0630

    Create settings.gradle.kts
    main settings.gradle.kts for all projects

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        kotlin("multiplatform").version(extra["kotlin.version"] as String)
+        kotlin("android").version(extra["kotlin.version"] as String)
+        id("com.android.application").version(extra["agp.version"] as String)
+        id("com.android.library").version(extra["agp.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Project Name"
+
+include(":android", ":desktop", ":common")
+
+
+
